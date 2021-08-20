1164b133368026c719356e56c82d094833064160
Devzila <nilay@devzila.com>
2021-08-20 11:56:23 +0530

    Update build.php

--- build.php
+++ build.php
@@ -4,7 +4,18 @@
     <h1> Output: </h1>
     
 <?php
-echo shell_exec("git pull origin main");
+echo '<pre>';
+
+// Outputs all the result of shellcommand "ls", and returns
+// the last output line into $last_line. Stores the return value
+// of the shell command in $retval.
+$last_line = system('git pull origin main', $retval);
+
+// Printing additional info
+echo '
+</pre>
+<hr />Last line of the output: ' . $last_line . '
+<hr />Return value: ' . $retval;
 ?>
   </body>
 </html>  
